5475eaf56309ace16a8940257e33faf9e484dfa8
Henry J Schmale <hschmale16@gmail.com>
2017-07-10 22:11:26 -0400

    RE Test Stuff

--- testprogs/RequirementEngineTest.cpp
+++ testprogs/RequirementEngineTest.cpp
@@ -1,9 +1,14 @@
 #include "RequirementEngine.hpp"
+#include "Player.hpp"
 #include "Misc.h"
+#include <list>
 
 /** A quick and dirty program to fiddle with the requirements engine
  */
 
 int main(int argc, char** argv) {
+    std::list<RequirementEngine> res;
+    std::list<Player> players;
 
+    return 0;
 }
